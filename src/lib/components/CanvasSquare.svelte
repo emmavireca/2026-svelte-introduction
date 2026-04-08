7540cf4ce9660a28ed79d2678c7ed3a513ae2f82
emmavireca <emmavireca@gmail.com>
2026-04-08 17:05:34 +0200

    add components that need $effect

---
+++ src/lib/components/CanvasSquare.svelte
@@ -0,0 +1,39 @@
+<script>
+    let size = $state(50)
+    let color = $state ("aff13e00")
+    
+</script>
+
+<h3>Canvas Square</h3>
+
+<article>
+     <!-- //spazio vuoto dove dsegni ogni pixel// -->
+    <canvas width ="100" height="100"></canvas> 
+    <nav>
+       <label>
+        Size: <input type="range">
+       </label>
+
+       <label>
+        Color: <input type="color">
+       </label>
+    </nav>
+</article>
+
+<style>
+ article {
+    display: flex;
+    gap: 1em;
+    align-items: end;
+ }
+
+ canvas {
+    border: solid 1px black;
+ }
+ 
+ nav {
+    display: flex;
+    gap: 1em;
+
+ }
+</style>
