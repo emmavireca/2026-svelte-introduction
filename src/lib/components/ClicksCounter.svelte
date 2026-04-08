8f44e932822c2044b6aaa0e2b5e4d7c0190cdec0
emmavireca <emmavireca@gmail.com>
2026-04-08 15:49:53 +0200

    customise components whit $props

---
+++ src/lib/components/ClicksCounter.svelte
@@ -0,0 +1,15 @@
+<script>
+    let props = $props()
+
+    let count = $state(props.initialCount || 0)
+    let doubled = $derived(count * 2)
+</script>
+
+<h3>Clicks Counter</h3>
+
+<button onclick={() => count++}>
+   Clicks: {count}
+</button>
+
+<span> doubled {doubled}</span>
+
